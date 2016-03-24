f5e074992921eeb03cad9f3a33139c8529d9e9a4
dbolivar <demian054m@gmail.com>
2016-03-24 10:45:03 -0430

    fase 1 estable

--- src/java/com/apcb/interfaceWeb/services/AirTcketService.java
+++ src/java/com/apcb/interfaceWeb/services/AirTcketService.java
@@ -33,6 +33,7 @@ import com.apcb.utils.ticketsHandler.entities.APCB_Itinerary;
 import com.apcb.utils.ticketsHandler.entities.APCB_Passenger;
 import com.apcb.utils.ticketsHandler.entities.APCB_Travel;
 import com.apcb.utils.ticketsHandler.enums.UniqueIDTypeEnum;
+import com.apcb.utils.utils.SessionIdentifierGenerator;
 import com.google.gson.Gson;
 import java.util.Calendar;
 import javax.ws.rs.DefaultValue;
@@ -63,6 +64,7 @@ public class AirTcketService {
     //@Path("/getAirTicket")
     public String getAirTicket(//@PathParam("request") String strRequest
         @DefaultValue("0") @QueryParam("sesionId") String sesionId,
+        @DefaultValue("NONE") @QueryParam("sesionIP") String sesionIP,
         @DefaultValue("true") @QueryParam("withReturn") boolean withReturn,
         @DefaultValue("") @QueryParam("departure_airport") String departure_airport,
         @DefaultValue("") @QueryParam("arrival_airport") String arrival_airport,
@@ -71,15 +73,19 @@ public class AirTcketService {
         @DefaultValue("0") @QueryParam("passengers_CNN") Integer passengers_CNN,
         @DefaultValue("0") @QueryParam("passengers_INF") Integer passengers_INF,
         @DefaultValue("ECO") @QueryParam("CabinType") String CabinType,
-        @DefaultValue("12") @QueryParam("transactionId") Integer transactionId){
+        @DefaultValue("1") @QueryParam("transactionId") String transactionId){
+        MDC.put("sessionId", sesionId);
+        MDC.put("sessionIP", sesionIP);
         log.info("AirTcketService -> getAirTicket ini");
         
         Response response;
         Request request = new Request();
+        request.setSesionId(sesionId);
         try { 
+            //request.setSesionId(SessionIdentifierGenerator.nextSessionId());
             APCB_Travel travel = new APCB_Travel();
             
-            travel.setTransactionId(transactionId);
+            travel.setTransactionId(sesionId);
 
             travel.setCabin(CabinTypeEnum.getByCode(CabinType));
             travel.setDirectFlightsOnly(true);
@@ -116,8 +122,6 @@ public class AirTcketService {
             passanger.setPassangerQuantity(passengers_INF);
             travel.putPassangers(passanger);
             
-            
-            request.setSesionId(sesionId);
             log.info(gson.toJson(request.getSesionId()));
             request.setMessage(new Message(MessagesTypeEnum.Ok));
             log.info(gson.toJson(request.getMessage()));
@@ -150,142 +154,6 @@ public class AirTcketService {
         Response response;
         try {
             request = new Request(strRequest);
-            /*APCB_Travel travel = new APCB_Travel();
-             Calendar calendar = Calendar.getInstance();
-
-            travel.setCabin(CabinTypeEnum.Economy);
-            travel.setDirectFlightsOnly(true);
-            
-            travel.setTransactionId(1000);
-            
-            APCB_Itinerary itinerary = new APCB_Itinerary();
-            
-            APCB_ItineraryOption itineraryOptionIDA = new APCB_ItineraryOption();
-            itineraryOptionIDA.setCabin(CabinTypeEnum.Economy);
-            
-            itineraryOptionIDA.setFlightNumber("301");
-            
-            calendar.set(5, 05, 2016 ,9,40,0);
-            itineraryOptionIDA.setDepartureDateTime(calendar);
-            
-            calendar.set(5, 05, 2016 ,10,40,0);
-            itineraryOptionIDA.setArrivalDateTime(calendar);
-            
-            itineraryOptionIDA.setDepartureLocationCode(LocationEnum.CCS);
-            itineraryOptionIDA.setArrivalLocationCode(LocationEnum.PMV);
-            
-            itinerary.putItineraryOption(itineraryOptionIDA);
-            
-            APCB_ItineraryOption itineraryOptionREG = new APCB_ItineraryOption();
-            itineraryOptionREG.setCabin(CabinTypeEnum.Economy);
-            itineraryOptionREG.setFlightNumber("303");
-            
-            calendar.set(5, 05, 2016 ,9,40,0);
-            itineraryOptionREG.setDepartureDateTime(calendar);
-            
-            calendar.set(5, 05, 2016 ,10,40,0);
-            itineraryOptionREG.setArrivalDateTime(calendar);
-            
-            itineraryOptionREG.setDepartureLocationCode(LocationEnum.CCS);
-            itineraryOptionREG.setArrivalLocationCode(LocationEnum.PMV);
-            
-            itinerary.putItineraryOption(itineraryOptionREG);
-            
-            travel.putItinerary(itinerary);
-            
-
-            APCB_Passenger passanger = new APCB_Passenger();
-            passanger.setPassangerType(PassangerTypeEnum.Adulto);
-            passanger.setPassangerQuantity(2);
-            
-            APCB_PassengerDetail passengerDetailADT1 = new APCB_PassengerDetail();
-            passengerDetailADT1.setName("PEPEa");
-            passengerDetailADT1.setSurname("TRUEnoa");
-            passengerDetailADT1.setDocumentType(DocumentTypeEnum.Cedula);
-            passengerDetailADT1.setDocumentID("123123123");
-            passengerDetailADT1.setEmail("Ergue@bito.com");
-            
-            passanger.putPassengersDetail(passengerDetailADT1);
-            
-            APCB_PassengerDetail passengerDetailADT2 = new APCB_PassengerDetail();
-            passengerDetailADT2.setName("PEPEb");
-            passengerDetailADT2.setSurname("TRUEnob");
-            passengerDetailADT2.setDocumentType(DocumentTypeEnum.Pasaporte);
-            passengerDetailADT2.setDocumentID("SAA123124");
-            passengerDetailADT2.setEmail("Ergue@bito2.com");
-            
-            passanger.putPassengersDetail(passengerDetailADT2);
-
-            travel.putPassangers(passanger);
-            
-            passanger = new APCB_Passenger();
-            passanger.setPassangerType(PassangerTypeEnum.Infante);
-            passanger.setPassangerQuantity(2);
-            
-            APCB_PassengerDetail passengerDetailINF1 = new APCB_PassengerDetail();
-            passengerDetailINF1.setName("PEPEaa");
-            passengerDetailINF1.setSurname("TRUEnoa");
-            passengerDetailINF1.setDocumentType(DocumentTypeEnum.Cedula);
-            passengerDetailINF1.setDocumentID("123123123");
-            passengerDetailINF1.setEmail("Ergue@bito.com");
-            
-            passanger.putPassengersDetail(passengerDetailINF1);
-            
-            APCB_PassengerDetail passengerDetailINF2 = new APCB_PassengerDetail();
-            passengerDetailINF2.setName("PEPEba");
-            passengerDetailINF2.setSurname("TRUEnob");
-            passengerDetailINF2.setDocumentType(DocumentTypeEnum.Cedula);
-            passengerDetailINF2.setDocumentID("SAA123124");
-            passengerDetailINF2.setEmail("Ergue@bito2.com");
-            
-            passanger.putPassengersDetail(passengerDetailINF2);
-
-            travel.putPassangers(passanger);
-            
-            passanger = new APCB_Passenger();
-            passanger.setPassangerType(PassangerTypeEnum.Niño);
-            passanger.setPassangerQuantity(1);
-            
-            APCB_PassengerDetail passengerDetailNIN1 = new APCB_PassengerDetail();
-            passengerDetailNIN1.setName("PEPEnin");
-            passengerDetailNIN1.setSurname("TRUEnoa");
-            passengerDetailNIN1.setDocumentType(DocumentTypeEnum.Cedula);
-            passengerDetailNIN1.setDocumentID("123123123");
-            passengerDetailNIN1.setEmail("Ergue@bito.com");
-            
-            passanger.putPassengersDetail(passengerDetailNIN1);
-            
-            travel.putPassangers(passanger);
-     
-            request.setSesionId("123");
-            log.info(gson.toJson(request.getSesionId()));
-            request.setMessage(new Message(MessagesTypeEnum.Ok));
-            log.info(gson.toJson(request.getMessage()));
-            //request.setBeam(gson.toJson(itinerary), Itinerary.class);
-            request.setTravelInfo(travel);
-            
-            APCB_PayMain payMainInfo = new APCB_PayMain();
-            payMainInfo.setAmount(new Double("10.00"));
-            payMainInfo.setDescription("Prueba");
-            payMainInfo.setCardHolder("Demian Bolivar");
-            payMainInfo.setCardHolderID("17498122");
-            payMainInfo.setCardNumber(new Long("4111111111111111"));
-            payMainInfo.setCVC(new Integer("123"));
-
-            payMainInfo.setExpirationDate("10-20");
-
-            
-            payMainInfo.setIP("192.168.1.108");
-
-            payMainInfo.setOrderNumber(2);
-            
-            // ---------------------------
-            
-                        
-            payMainInfo.setPaymentType(PaymentTypeEnum.TarjetaCredito);
-            payMainInfo.setTaxesAmount(new Double("0"));
-
-            request.setPayMainInfo(payMainInfo);*/
             log.info("requestTotal ticketReservAndPay->"+gson.toJson(request));
              
             APCBInterfaceWebProcess process = new APCBInterfaceWebProcess();
@@ -308,7 +176,10 @@ public class AirTcketService {
     @GET
     @Produces("text/plain")
     @Path("/getConsultReservOrTicket")
-    public String getConsultReservOrTicket(@QueryParam("type") String strType, @QueryParam("code") String strCode) {
+    public String getConsultReservOrTicket(
+            @DefaultValue("0") @QueryParam("sesionId") String sesionId,
+            @QueryParam("type") String strType, 
+            @QueryParam("code") String strCode) {
         Request request = new Request();
         Response response;
         try {
